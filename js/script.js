2b9275e196a9c1fa9436d9101218ea86357d45b1
TANS123 <44278831+TANS123@users.noreply.github.com>
2019-11-06 18:29:52 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,10 +10,36 @@
 // DOCUMENT READY FUNCTION BELOW
 
 $("#shoot").click(function(){
-    var rps = $("#input").val();
-    if (rps === "rock"){
-        $("#userChoice").text(rps);
-        $("#computerChoice").text("scissors");
-        // } else if (rps === scissors)
+  var computer = Math.random();
+  var rps = $("#input").val();
+    console.log(rps);
+      $("#userChoice").text(rps);
+    if (computer > 2/3) {
+      $("#computerChoice").text('rock');
+    } else if (computer > 1/3 ) {
+      $('#computerChoice').text('scissors');
+    } else {
+      $('#computerChoice').text('paper');
+    } 
+    if (rps === "rock" & computer === "paper") {
+      $('#winner').text("Computer won");
+    } else if (rps === "rock" & computer === "scissors") {
+      $('#winner').text("You won");
+    } else if (rps === "rock" & computer === "rock") {
+      $('#winner').text("It's a tie");
+    } else if (rps === "paper" & computer === "paper") {
+      $('#winner').text("It's a tie");
+    } else if (rps === "paper" & computer === "rock") {
+      $('#winner').text("You won");
+    } else if (rps === "paper" & computer === "scissors") {
+      $('#winner').text("Computer won");
+    } else if (rps === "scissors" & computer === "paper") {
+      $('#winner').text("You won");
+    } else if (rps === "rock" & computer === "paper") {
+      $('#winner').text("Computer won");
+    } else if (rps === "scissors" & computer === "scissors") {
+      $('#winner').text("It's a tie");
     }
+    
 });
+
